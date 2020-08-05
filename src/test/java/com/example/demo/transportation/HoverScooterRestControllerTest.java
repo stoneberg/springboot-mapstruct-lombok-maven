aa32dd3e537854c2af292e060d1f0d47f6f9a006
stoneberg <stoneberg73@gmail.com>
2020-08-06 03:31:34 +0900

    :)

--- src/test/java/com/example/demo/transportation/HoverScooterRestControllerTest.java
+++ src/test/java/com/example/demo/transportation/HoverScooterRestControllerTest.java
@@ -1,22 +1,29 @@
 package com.example.demo.transportation;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
-public class HoverScooterRestControllerTest {
+class HoverScooterRestControllerTest {
+    
+    private static final Logger log = LoggerFactory.getLogger(HoverScooterRestControllerTest.class);
+    
     @Autowired
     HoverScooterRestController hoverScooterRestController;
 
     @Test
-    public void scooter() {
+    @DisplayName("두개의 객체를 조합하여 결과를 도출")
+    void get_scooter_test() {
         HoverScooter scooter = hoverScooterRestController.scooter();
-        System.out.println(scooter);
-        assertEquals("Mike", scooter.getMadeforalien());
-        assertEquals(2500.0d, scooter.getMaxSpeed());
-        assertEquals("T800_SlimyAlien", scooter.getModel());
+        log.info("@scooter===========>{}", scooter);
+        assertThat(scooter.getMadeforalien()).isEqualTo("Mike");
+        assertThat(scooter.getMaxSpeed()).isEqualTo(2500.0d);
+        assertThat(scooter.getModel()).isEqualTo("T800_SlimyAlien");
     }
 }
